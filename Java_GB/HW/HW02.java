b193e609b0e34660b06aaddd203e8e56905e4e2d
Nikita Sharov <lliapik@gmail.com>
2023-12-04 14:02:52 +0300

    printPrimeNums

--- Java_GB/HW/HW02.java
+++ Java_GB/HW/HW02.java
@@ -1,7 +1,7 @@
 package Java_GB.Java_GB.HW;
 
-//Напишите функцию printPrimeNums, которая выведет на экран все простые числа в промежутке от 1 до 1000, каждое на новой строке.
-//Напишите свой код в методе printPrimeNums класса Answer.
+// Напишите функцию printPrimeNums, которая выведет на экран все простые числа в промежутке от 1 до 1000, каждое на новой строке.
+// Напишите свой код в методе printPrimeNums класса Answer.
 
 public class HW02 {
     public static void main(String[] args) {
