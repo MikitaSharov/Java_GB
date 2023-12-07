46a12841b7ea67f3d6753b3670dca1c9b2122fb6
Nikita Sharov <lliapik@gmail.com>
2023-12-07 13:07:31 +0300

    list.remove instanceof Integer, list.iterator

--- Task014.java
+++ Task014.java
@@ -3,9 +3,10 @@ package Java_GB;
 //Создать список типа ArrayList<String>. Поместить в него как строки, так и целые числа. Пройти по списку, найти и удалить целые числа.
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class Main01 {
+public class Task014 {
     public static void main(String[] args) {
         List list = new ArrayList<String>();
         list.add("1");
@@ -20,5 +21,13 @@ public class Main01 {
         System.out.println(list);
     }
 
-
+    static void removeV2(List list){
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext()){
+            Object item = iterator.next();
+            if (item instanceof Integer){
+                iterator.remove();
+            }
+        }
+    }
 }
